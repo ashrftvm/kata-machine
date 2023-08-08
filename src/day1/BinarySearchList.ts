5809fb0dc9705c8204baceec1d96fb95b0c312ad
Ashrf <ashrftvm1@gmail.com>
2023-08-09 00:47:23 +0400

    binary search implementation

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
 
+    do {
+        const mid = Math.floor(low + (high - low) / 2);
+        const value = haystack[mid];
+        if (value === needle) {
+            return true;
+        } else if (needle > value) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    } while (high > low);
+
+    return false;
 }
