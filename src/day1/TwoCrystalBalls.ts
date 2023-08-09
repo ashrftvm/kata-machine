1b77e3df49e28a8e3d21504b149fa48b53e9d782
Ashrf <ashrftvm1@gmail.com>
2023-08-09 19:19:02 +0400

    implemented two crystal ball problem with sqrt

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,21 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jmpNumber = Math.floor(Math.sqrt(breaks.length / 2));
 
+    let i = jmpNumber;
+
+    for (; i < breaks.length; i += jmpNumber) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    i -= jmpNumber;
+
+    for (let j = 0; j <= jmpNumber && i < breaks.length; j++, i++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
 }
