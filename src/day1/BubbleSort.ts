c23cc5120172ca9df5bfa246780b371f596e9b4f
Ashrf <ashrftvm1@gmail.com>
2023-08-10 19:08:40 +0400

    implemented bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,12 @@
 export default function bubble_sort(arr: number[]): void {
-
+    let length = arr.length;
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
 }
