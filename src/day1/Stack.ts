885edcbfc8da0483ca1aa3c45be10e986c19633b
Ashrf <ashrftvm1@gmail.com>
2023-08-16 12:45:01 +0400

    working model of stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,39 @@
+type Node<T> = {
+    prev?: Node<T> | undefined
+    value: T
+}
 export default class Stack<T> {
     public length: number;
+    public head: Node<T> | undefined;
+    public prev: Node<T> | undefined;
 
-    
 
     constructor() {
+        this.head = this.prev = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
-
-}
+        this.length++;
+        const newItem = {
+            prev: this.head,
+            value: item
+        }
+        this.head = newItem;
+    }
     pop(): T | undefined {
-
-}
+        if(!this.head){
+            return undefined
+        }
+        const head = this.head
+        this.head = head?.prev;
+        this.length --;
+        if(this.length === 0){
+            this.head = this.prev = undefined
+        }
+        return head?.value
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
