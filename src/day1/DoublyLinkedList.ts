f23d344546b438fe450732064b26cca6d920c345
Ashrf <ashrftvm1@gmail.com>
2023-08-22 10:30:59 +0400

    remove op doubly linked list

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -69,7 +69,43 @@ export default class DoublyLinkedList<T> {
         this.tail = node
     }
     remove(item: T): T | undefined {
+        let curr = this.head
+        for (let i = 0; curr && i < this.length; i++) {
+            if (curr.value === item) {
+                break
+            }
+            curr = curr.next
+        }
 
+        if(!curr){
+            return undefined
+        }
+        
+        this.length--;
+        if (this.length === 0) {
+            const out = this.head?.value
+            this.head = this.tail = undefined;
+            return out
+        }
+
+        if(curr.prev){
+            curr.prev = curr.next
+        }
+
+        if(curr.next){
+            curr.next = curr.prev
+        }
+
+        if(curr === this.head){
+            this.head = curr.next
+        }
+
+        if(curr === this.tail){
+            this.tail = curr.prev
+        }
+
+        curr.next = curr.prev = undefined
+        return curr.value
     }
     get(idx: number): T | undefined {
 
