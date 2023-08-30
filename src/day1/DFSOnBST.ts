22765f9a486301712b4a519a413a39f37acc4cf2
Ashrf <ashrftvm1@gmail.com>
2023-08-31 00:33:42 +0400

    DFS find binary search tree

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,12 @@
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    if (!head) return false;
 
+    if (head.value === needle) return true;
+
+    if (head.value < needle) {
+        if (head.right) return dfs(head.right, needle);
+        return false;
+    }
+    if (head.left) return dfs(head.left, needle);
+    return false;
 }
