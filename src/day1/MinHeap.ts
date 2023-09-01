966ba124573977d9e7d1b1c6d85ab8f2d27399b7
Ashrf <ashrftvm1@gmail.com>
2023-09-01 11:27:37 +0400

    min heap initilization

--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -1,15 +1,24 @@
 export default class MinHeap {
     public length: number;
-
-    
+    private data: number[];
 
     constructor() {
+        this.data = [];
+        this.length = 0;
     }
 
-    insert(value: number): void {
+    private parent = (idx: number): number => {
+        return Math.floor((idx - 1) / 2);
+    };
 
-}
-    delete(): number {
+    private leftChild = (idx: number): number => {
+        return 2 * idx + 1;
+    };
 
-}
+    private rightChild = (idx: number): number => {
+        return 2 * idx + 2;
+    };
+
+    insert(value: number): void {}
+    delete(): number {}
 }
