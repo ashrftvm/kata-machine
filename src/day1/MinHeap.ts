0f5a5d4a50378e2b181bf57d534292dafce81f38
Ashrf <ashrftvm1@gmail.com>
2023-09-02 13:26:10 +0400

    heapify up implementation

--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -7,6 +7,20 @@ export default class MinHeap {
         this.length = 0;
     }
 
+    private heapifyUp(idx: number): void {
+        if (idx === 0) return;
+
+        const p = this.parent(idx);
+        const parentV = this.data[p];
+        const v = this.data[idx];
+
+        if (parentV > v) {
+            this.data[idx] = parentV;
+            this.data[p] = v;
+            this.heapifyUp(p);
+        }
+    }
+
     private parent = (idx: number): number => {
         return Math.floor((idx - 1) / 2);
     };
